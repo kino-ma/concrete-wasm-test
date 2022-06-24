d8eeb64206397d9fd780f41ae9979bb359ff4953
kino-ma <ma@kino.ma>
2022-06-24 19:53:29 +0900

    [build] use software seeder

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 use std::panic;
 
 extern crate console_error_panic_hook;
-use concrete_csprng::generators::{RandomGenerator, SoftwareRandomGenerator};
-use concrete_csprng::seeders::{ExternalLibSeeder, Seeder};
+use concrete::*;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -14,27 +13,38 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn generate() {
-    log("enter");
-    panic::set_hook(Box::new(console_error_panic_hook::hook));
-    log("set hook");
-    let mut seeder = ExternalLibSeeder;
-    log("seeder");
-    let seed = seeder.seed();
-    log("seed");
-    let mut generator = SoftwareRandomGenerator::new(seed);
-    log("generator");
-    let number = generator.next().unwrap();
-    log("generate");
-    alert(&format!("generated: {}", number));
-    log("return");
+pub fn run() {
+    main().expect("failed to run concrete");
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+fn main() -> Result<(), CryptoAPIError> {
+    // generate a secret key
+    let secret_key = LWESecretKey::new(&LWE128_630);
+
+    // the two values to add
+    let m1 = 8.2;
+    let m2 = 5.6;
+
+    // specify the range and precision to encode messages into plaintexts
+    // here we encode in [0, 10[ with 8 bits of precision and 1 bit of padding
+    let encoder = Encoder::new(0., 10., 8, 1)?;
+
+    // encode the messages into plaintexts
+    let p1 = encoder.encode_single(m1)?;
+    let p2 = encoder.encode_single(m2)?;
+
+    // encrypt plaintexts
+    let mut c1 = VectorLWE::encrypt(&secret_key, &p1)?;
+    let c2 = VectorLWE::encrypt(&secret_key, &p2)?;
+
+    // add the two ciphertexts homomorphically
+    c1.add_with_padding_inplace(&c2)?;
+
+    // decrypt and decode the result
+    let m3 = c1.decrypt_decode(&secret_key)?;
+
+    // print the result and compare to non-FHE addition
+    log(&format!("Real: {} + {} = {}", m1, m2, m1 + m2));
+    log(&format!("FHE: {} + {} = {}", p1.decode()?[0], p2.decode()?[0], m3[0]));
+    Ok(())
 }
